7bafd371fc22e21ec1b2dcc6d6a2b9df97c15766
Your Name <you@example.com>
2019-02-02 20:29:56 -0500

    add non-interactive. needs to wait for second entry

--- startup.sh
+++ startup.sh
@@ -3,9 +3,12 @@
 useradd -ms /bin/bash ${USER}
 echo ${USER}":"${PASSWD}|chpasswd
 su - ${USER} -c \
-   ' \
+'
+echo "test
+123456
+123456" | \
 /opt/google/chrome-remote-desktop/start-host		                \
-   --code=${CODE}		                     	                \
-   --redirect-url="https://remotedesktop.google.com/_/oauthredirect"    \
-   --name=${NAME}                                                       \
-   '
+--code=${CODE}		                     	                	    \
+--redirect-url="https://remotedesktop.google.com/_/oauthredirect"	\
+--name=${NAME}                                                   	\
+'
